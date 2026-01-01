01450b44b9ab226caa8b707b9f231fcd30e9f28f
Mustafa Espada <kopukfad@gmail.com>
2026-01-01 13:38:02 +0300

    ver2.1

--- test_giris.py
+++ test_giris.py
@@ -1,15 +1,10 @@
-from selenium import webdriver  # <--- Bu satır olmazsa çalışmaz!
+from selenium import webdriver
 import time
 
-# 1. Chrome'u aç
 driver = webdriver.Chrome()
-
-# 2. Siteye git
+driver.maximize_window()
 driver.get("http://www.seyyahlab.com")
 
-# 3. 10 saniye bekle
 time.sleep(10)
-
-# 4. Kapat
 driver.quit()
 
