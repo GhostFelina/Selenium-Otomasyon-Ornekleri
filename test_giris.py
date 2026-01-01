672356907fe37425a65d801c453743f78da9ba84
Mustafa Espada <kopukfad@gmail.com>
2026-01-01 13:17:00 +0300

    ver2

--- test_giris.py
+++ test_giris.py
@@ -1,14 +1,20 @@
-from selenium import webdriver
+from selenium import webdriver  # <--- Bu satır olmazsa çalışmaz!
 import time
 
 # 1. Chrome'u aç
 driver = webdriver.Chrome()
 
-# 2. Google'a git
-driver.get("https://www.google.com")
+# 2. Siteye git
+driver.get("http://www.seyyahlab.com")
 
-# 3. 5 saniye bekle (görebilmemiz için)
-time.sleep(5)
+# 3. 10 saniye bekle
+time.sleep(10)
 
 # 4. Kapat
 driver.quit()
+
+from selenium import webdriver
+import time
+
+driver = webdriver.Chrome()
+driver.get("http://www.seyyahlab.com")
